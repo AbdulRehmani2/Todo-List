005ad6c141991130692d502b7354948a4fa58b87
AbdulRehmani2 <abdulrehmani2238@gmail.com>
2024-06-24 14:02:50 +0500

    Added hide option to button

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,14 +1,18 @@
+import { useState } from "react";
+
 interface ListItem{
     text: string;
 }
 
 function ListItem({ text }: ListItem)
 {
+    const [isDone, setIsDone] = useState(false)
+
     return(
         <div className="item-container">
-            <input type="checkbox" name="isdone" id="isdone" />
+            <input type="checkbox" name="isdone" id="isdone" onChange={() => setIsDone(prev => !prev)}/>
             <span>{text}</span>
-            <button>X</button>
+            <button className={isDone ? "hide" : ""}>X</button>
         </div>
     )
 }
